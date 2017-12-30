ad551f98e45b0988d51d284a85c1b711de68faa3
NARESHK <naresh.kumar@aboveo.com>
2017-12-30 11:12:26 -0500

    Added database

--- TaskTrackerProject/grails-app/conf/DataSource.groovy
+++ TaskTrackerProject/grails-app/conf/DataSource.groovy
@@ -19,7 +19,7 @@ environments {
     development {
         dataSource {
             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:F:/Naresh/projectDB/sample;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            url = "jdbc:h2:sample;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
 			username = "sa"
 			password = ""
 			properties {
